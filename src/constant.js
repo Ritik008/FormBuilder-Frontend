9791be881b14ae3dba0c103171c46c13420c069e
ritik008 <ritikthakur27rt@gmail.com>
2024-02-25 23:52:34 +0530

    change

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL='//ec2-52-66-253-190.ap-south-1.compute.amazonaws.com:8000'
+export const BASE_URL='https://formbuilder-backend.onrender.com'
 // export const BASE_URL='http://localhost:8000'
