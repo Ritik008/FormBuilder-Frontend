9ed0e0be7f31d234bea206f5063df55c4a9ac9b0
ritik008 <ritikthakur27rt@gmail.com>
2024-02-25 23:45:51 +0530

    changes

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL='http://ec2-52-66-253-190.ap-south-1.compute.amazonaws.com:8000'
+export const BASE_URL='//ec2-52-66-253-190.ap-south-1.compute.amazonaws.com:8000'
 // export const BASE_URL='http://localhost:8000'
